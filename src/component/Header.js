b2c51805aba2ab1244d867bee2e1af433833ab44
maricon-fullstack <maricon@fullstack.ph>
2021-02-08 14:33:58 +0800

    Fixed amend

--- src/component/Header.js
+++ src/component/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="header-content">
           <Link to="/" className="navbar-logo">
             <img src={siteLogo} className="logo" alt="Billsby" />
-            <span className="t-logo">Card Tokenizer</span>
+            <span className="t-logo">Billsby Card Tokenizer (React)</span>
           </Link>
           <button className={`toggle-menu burger-close ${toggleMenu && 'burger-open'}`} onClick={handleToggleMenu}>
             <span className="icon-bar"></span>
@@ -23,7 +23,7 @@ const Header = () => {
             <span className="icon-bar"></span>
           </button>
           <div className={`menu-holder ${toggleMenu && 'menu-open'}`}>
-            <a href="https://github.com/Billsby/billsby-tokenizer-react/archive/master.zip" className="btn-white" download>Download on Github</a>
+            <a href="https://github.com/Billsby/billsby-tokenizer-react" className="btn-white">Download on Github</a>
           </div>
         </div>
       </div>
