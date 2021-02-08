3766bcdda9dce150de29024b48abfe1fa1f31814
maricon-fullstack <maricon@fullstack.ph>
2021-02-08 14:35:18 +0800

    Fixed amend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'react-json-pretty/themes/monikai.css';
 import JSONPretty from 'react-json-pretty';
 
